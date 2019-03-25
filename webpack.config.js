928487baff365a43073b449b3f3f3922a2032f56
Aliaksandr Banuzau <Aliaksandr_Banuzau@epam.com>
2019-03-25 17:54:41 +0300

    HW-2 fix using `mode` variable

--- webpack.config.js
+++ webpack.config.js
@@ -7,34 +7,36 @@ const htmlPlugin = new HtmlWebPackPlugin({
 
 const OptimizeJsPlugin = require("optimize-js-plugin");
 
-var environment = process.env.NODE_ENV;
-
 var plugins = [htmlPlugin];
 
-if (environment === "production") {
-    plugins.push(new OptimizeJsPlugin({
-        sourceMap: false
-    }));
-}
-
-module.exports = {
-    entry: './src/index.jsx',
-
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
+module.exports = (env, argv) => {
     
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
+    if (argv.mode === "production") {
+        plugins.push(new OptimizeJsPlugin({
+            sourceMap: false
+        }));
+    }
+
+    return {
+
+        entry: './src/index.jsx',
+
+        resolve: {
+            extensions: ['.js', '.jsx']
+        },
+        
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
                 }
-            }
-        ]
-    },
-    //stats: "verbose",
-    plugins: plugins
+            ]
+        },
+        //stats: "verbose",
+        plugins: plugins
+    }
 };
